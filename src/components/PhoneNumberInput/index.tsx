d36988b8fff370412fc1673c6054f6ea34599751
Gigi Jishkariani <73842838+Jishkarianii@users.noreply.github.com>
2023-02-20 23:40:12 +0400

    Create PhoneNumberInput component

---
+++ src/components/PhoneNumberInput/index.tsx
@@ -0,0 +1,67 @@
+import "./style.scss";
+import { useState, useEffect } from "react";
+import { phoneCodeList } from "../../data/phoneNumberCode";
+import KeyboardArrowDownRoundedIcon from "@mui/icons-material/KeyboardArrowDownRounded";
+
+interface Props {
+  title: string;
+  state: string;
+  setState: (value: string) => void;
+  errorMsg: string;
+}
+
+function PhoneNumberInput({ title, state, setState, errorMsg }: Props) {
+  const [isFocused, setIsFocused] = useState<boolean>(false);
+  const [code, setCode] = useState<string>(phoneCodeList[230].dial_code);
+  const [number, setNumber] = useState<string>("");
+  const [phoneCode, setPhoneCode] = useState<string>(
+    `${phoneCodeList[230].code} ${phoneCodeList[230].dial_code}`
+  );
+
+  useEffect(() => {
+    setState(`${code}${number}`);
+  }, [code, number]);
+
+  return (
+    <div className="phone-number-input">
+      <p className="title">{title}</p>
+      <div
+        className={`select-phone-code ${isFocused ? "focused" : ""} ${
+          errorMsg ? "error" : ""
+        }`}
+        tabIndex={-1}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+      >
+        <div className="select-phone-code-head" tabIndex={-1}>
+          <span>{phoneCode}</span>
+          <KeyboardArrowDownRoundedIcon />
+        </div>
+        <input
+          type="text"
+          value={number}
+          placeholder="Phone Number"
+          onChange={(e: React.FormEvent<HTMLInputElement>) =>
+            setNumber(e.currentTarget.value)
+          }
+        />
+        <div className="select-phone-code-options">
+          {phoneCodeList.map((item, idx) => (
+            <p
+              key={idx}
+              onClick={() => {
+                setPhoneCode(`${item.code} ${item.dial_code}`);
+                setCode(item.dial_code);
+              }}
+            >
+              {item.code} {item.name} {item.dial_code}
+            </p>
+          ))}
+        </div>
+      </div>
+      {errorMsg && <p className="error-msg">{errorMsg}</p>}
+    </div>
+  );
+}
+
+export default PhoneNumberInput;
